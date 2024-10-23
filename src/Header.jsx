5c6188b474b21962f75fd04950713bbaadb3479e
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2024-10-23 08:40:14 +0600

    Movie & Cart Modal-Added

---
+++ src/Header.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Moon from "./assets/icons/moon.svg";
+import Logo from "./assets/logo.svg";
+import Ring from "./assets/ring.svg";
+import ShopingCart from "./assets/shopping-cart.svg";
+import CartDetails from "./cine/cartDetails";
+
+export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+  function handleCartShow() {
+    setShowCart(true);
+  }
+
+  return (
+    <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
+      <nav className="container flex items-center justify-between space-x-10 py-6">
+        <a href="index.html">
+          <img src={Logo} width="139" height="26" alt="logo" />
+        </a>
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={Ring} width="24" height="24" alt="ring" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={Moon} width="24" height="24" alt="moon" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+              onClick={handleCartShow}
+            >
+              <img src={ShopingCart} width="24" height="24" alt="cart" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
